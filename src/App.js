71ec273d888bb89f684c6b1425a3703662745f48
Sam Lui <business.samlui@gmail.com>
2022-12-09 17:24:00 +1300

    Deployed Website

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import TopBar from './components/TopBar';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Activities from './components/Activities';
+import Booking from './components/Booking';
+import Gallery from './components/Gallery';
+import Contact from './components/Contact';
+
+function App() {
+  return (
+    <div>
+      <TopBar />
+      <Navbar />
+      <Hero />
+      <Activities />
+      <Booking />
+      <Gallery />
+      <Contact />
+    </div>
+  );
+};
+
+
+export default App;
---
+++ src/components/Contact.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div id='#contact'>
+        <h2>Send us a message</h2>
+        <p>We're standing by!</p>
+        <div>
+            <img src='' alt='/' />
+        </div>
+    </div>
+  )
+}
+
+export default Contact
